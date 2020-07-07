a42fce992491af2bcacddc87403f7f56395cd24b
eXtremal-ik7 <eXtremal.ik7@gmail.com>
2020-07-07 21:41:39 +0300

    Backend initialization code added

---
+++ src/cmake/ProjectConfig4cpp.cmake
@@ -0,0 +1,35 @@
+include(ExternalProject)
+
+set(CONFIG4CPP_DIRECTORY "${CMAKE_SOURCE_DIR}/../dependencies/config4cpp")
+
+if (NOT MSVC)
+  ExternalProject_Add(config4cpp
+    GIT_REPOSITORY https://github.com/eXtremal-ik7/config4cpp
+    GIT_TAG master
+    GIT_SHALLOW 1
+    SOURCE_DIR ${CONFIG4CPP_DIRECTORY}
+    BINARY_DIR ${CONFIG4CPP_DIRECTORY}
+    CONFIGURE_COMMAND ""
+    BUILD_COMMAND ${CMAKE_MAKE_PROGRAM}
+    INSTALL_COMMAND ""
+    UPDATE_COMMAND ""
+  )
+else()
+  ExternalProject_Add(config4cpp
+    GIT_REPOSITORY https://github.com/eXtremal-ik7/config4cpp
+    GIT_TAG master
+    GIT_SHALLOW 1
+    SOURCE_DIR ${CONFIG4CPP_DIRECTORY}
+    BINARY_DIR ${CONFIG4CPP_DIRECTORY}/src
+    CONFIGURE_COMMAND ""
+    BUILD_COMMAND nmake -f Makefile.win config4cpp.lib
+    INSTALL_COMMAND ""
+    UPDATE_COMMAND ""
+  )
+endif()
+
+add_library(CONFIG4CPP_LIBRARY__ STATIC IMPORTED DEPENDS config4cpp)
+set_target_properties(CONFIG4CPP_LIBRARY__ PROPERTIES IMPORTED_LOCATION ${CONFIG4CPP_DIRECTORY}/lib/${CMAKE_STATIC_LIBRARY_PREFIX}config4cpp${CMAKE_STATIC_LIBRARY_SUFFIX})
+
+set(CONFIG4CPP_INCLUDE_DIR ${CONFIG4CPP_DIRECTORY}/include)
+set(CONFIG4CPP_LIBRARY CONFIG4CPP_LIBRARY__)
--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,127 @@
+#include "poolcore/backend.h"
+
+#include "config4cpp/Configuration.h"
+
 #include "asyncio/asyncio.h"
 #include "asyncio/socket.h"
 #include "loguru.hpp"
 
+#include <signal.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+#include <thread>
+
+static int interrupted = 0;
+static void sigIntHandler(int) { interrupted = 1; }
+
+struct PoolConfig {
+  bool IsMaster;
+  std::filesystem::path DatabasePath;
+  uint16_t HttpPort;
+
+  std::vector<PoolBackend> Backends;
+};
+
 int main(int argc, char *argv[])
 {
-  initializeSocketSubsystem();
+  if (argc != 2) {
+    fprintf(stderr, "Usage: %s <configuration file>\n", argv[0]);
+    return 1;
+  }
+
+  char logFileName[64];
+  {
+    auto t = time(nullptr);
+    auto now = localtime(&t);
+    snprintf(logFileName, sizeof(logFileName), "poolfrontend-%04u-%02u-%02u.log", now->tm_year + 1900, now->tm_mon + 1, now->tm_mday);
+  }
+
+  loguru::g_stderr_verbosity = loguru::Verbosity_OFF;
+  loguru::g_preamble_thread = true;
+  loguru::g_preamble_file = false;
+  loguru::g_flush_interval_ms = 100;
   loguru::init(argc, argv);
+  loguru::add_file(logFileName, loguru::Append, loguru::Verbosity_INFO);
+  loguru::g_stderr_verbosity = 1;
   loguru::set_thread_name("main");
-  
+
+  PoolBackendConfig backendConfig;
+  config4cpp::Configuration *cfg = config4cpp::Configuration::create();
+  PoolConfig poolConfig;
+
+  try {
+    cfg->parse(argv[1]);
+    const char *frontendSection = "poolfrontend";
+
+    poolConfig.IsMaster = cfg->lookupBoolean(frontendSection, "isMaster", true);
+    poolConfig.DatabasePath = cfg->lookupString(frontendSection, "dbPath");
+    poolConfig.HttpPort = cfg->lookupInt(frontendSection, "httpPort");
+
+    // Initialize user manager
+
+    // Initialize all backends
+    config4cpp::StringVector coinsList;
+    cfg->lookupList(frontendSection, "coins", coinsList);
+    for (int i = 0, ie = coinsList.length(); i != ie; ++i) {
+      std::string scopeName = std::string("coins.") + coinsList[i];
+      const char *scope = scopeName.c_str();
+
+      PoolBackendConfig backendConfig;
+      backendConfig.CoinName = coinsList[i];
+
+      // Inherited pool config parameters
+      backendConfig.isMaster = poolConfig.IsMaster;
+      backendConfig.dbPath = poolConfig.DatabasePath;
+
+      // Backend parameters
+      backendConfig.RequiredConfirmations = cfg->lookupInt(scope, "requiredConfirmations", 10);
+      backendConfig.DefaultMinimalPayout = static_cast<int64_t>(cfg->lookupFloat(scope, "defaultMinimalPayout", 0.01) * COIN);
+      backendConfig.MinimalPayout = static_cast<int64_t>(cfg->lookupFloat(scope, "minimalPayout", 0.001) * COIN);
+      backendConfig.KeepRoundTime = cfg->lookupInt(scope, "keepRoundTime", 3) * 24*3600;
+      backendConfig.KeepStatsTime = cfg->lookupInt(scope, "keepStatsTime", 2) * 60;
+      backendConfig.ConfirmationsCheckInterval = cfg->lookupInt(scope, "confirmationsCheckInterval", 10) * 60 * 1000000;
+      backendConfig.PayoutInterval = cfg->lookupInt(scope, "payoutInterval", 60) * 60 * 1000000;
+      backendConfig.BalanceCheckInterval = cfg->lookupInt(scope, "balanceCheckInterval", 3) * 60 * 1000000;
+      backendConfig.StatisticCheckInterval = cfg->lookupInt(scope, "statisticCheckInterval", 1) * 60 * 1000000;
+      // ZEC specific
+      backendConfig.poolZAddr = cfg->lookupString(scope, "pool_zaddr", "");
+      backendConfig.poolTAddr = cfg->lookupString(scope, "pool_taddr", "");
+
+      // Initialize backend
+      poolConfig.Backends.emplace_back(std::move(backendConfig));
+    }
+
+  } catch(const config4cpp::ConfigurationException& ex){
+    LOG_F(ERROR, "%s", ex.c_str());
+    exit(1);
+  }
+
+  initializeSocketSubsystem();
+  asyncBase *base = createAsyncBase(amOSDefault);
     
+  // Start backends for all coins
+  for (auto &backend: poolConfig.Backends) {
+    backend.start();
+  }
+
+  // Handle CTRL+C (SIGINT)
+  signal(SIGINT, sigIntHandler);
+  signal(SIGTERM, sigIntHandler);
+  std::thread sigIntThread([&base, &poolConfig]() {
+    while (!interrupted)
+      std::this_thread::sleep_for(std::chrono::seconds(1));
+    LOG_F(INFO, "Interrupted by user");
+    for (auto &backend: poolConfig.Backends) {
+      backend.stop();
+    }
+    postQuitOperation(base);
+  });
+
+  sigIntThread.detach();
+
+  asyncLoop(base);
+  LOG_F(INFO, "poolfrondend stopped\n");
   return 0;
 }

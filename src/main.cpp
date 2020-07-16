c62d580a8fd3069598dc0b40adc32b1d640a26ca
eXtremal-ik7 <eXtremal.ik7@gmail.com>
2020-07-16 15:13:06 +0300

    Update config format; run 'getblocktemplate' polling on startup

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,8 @@
 #include "http.h"
 #include "poolcore/backend.h"
+#include "poolcore/bitcoinRPCClient.h"
 #include "poolcore/coinLibrary.h"
+#include "poolcore/clientDispatcher.h"
 #include "poolcommon/utils.h"
 
 #include "config4cpp/Configuration.h"
@@ -30,11 +32,13 @@ struct PoolContext {
   std::filesystem::path DatabasePath;
   uint16_t HttpPort;
 
-  std::unique_ptr<PoolHttpServer> HttpServer;
-  std::unique_ptr<UserManager> UserMgr;
-  std::vector<PoolBackend> Backends;
   std::vector<CCoinInfo> CoinList;
+  std::vector<std::unique_ptr<CNetworkClientDispatcher>> ClientsDispatcher;
   std::unordered_map<std::string, size_t> CoinIdxMap;
+  std::vector<PoolBackend> Backends;
+
+  std::unique_ptr<UserManager> UserMgr;
+  std::unique_ptr<PoolHttpServer> HttpServer;
 };
 
 void InitializeWorkerThread()
@@ -63,7 +67,7 @@ int main(int argc, char *argv[])
 
   loguru::g_stderr_verbosity = loguru::Verbosity_OFF;
   loguru::g_preamble_thread = true;
-  loguru::g_preamble_file = false;
+  loguru::g_preamble_file = true;
   loguru::g_flush_interval_ms = 100;
   loguru::init(argc, argv);
   loguru::add_file(logFileName, loguru::Append, loguru::Verbosity_INFO);
@@ -73,16 +77,33 @@ int main(int argc, char *argv[])
   PoolBackendConfig backendConfig;
   config4cpp::Configuration *cfg = config4cpp::Configuration::create();
   PoolContext poolContext;
+  unsigned totalThreadsNum = 0;
   unsigned workerThreadsNum = 0;
+
+  initializeSocketSubsystem();
+  asyncBase *base = createAsyncBase(amOSDefault);
 
   try {
     cfg->parse(argv[1]);
     const char *frontendSection = "poolfrontend";
+    config4cpp::StringVector coinsList;
 
     poolContext.IsMaster = cfg->lookupBoolean(frontendSection, "isMaster", true);
     poolContext.DatabasePath = cfg->lookupString(frontendSection, "dbPath");
     poolContext.HttpPort = cfg->lookupInt(frontendSection, "httpPort");
     workerThreadsNum = cfg->lookupInt(frontendSection, "workerThreadsNum", 0);
+    if (workerThreadsNum == 0)
+      workerThreadsNum = std::thread::hardware_concurrency() ? std::thread::hardware_concurrency() : 4;
+
+    cfg->lookupList(frontendSection, "coins", coinsList);
+
+    // Calculate total threads Num
+    totalThreadsNum =
+      1 +                  // Main thread
+      1 +                  // Listeners and clients polling
+      workerThreadsNum +   // Share checkers
+      coinsList.length() + // Backends
+      1;                   // HTTP server
 
     // Initialize user manager
     poolContext.UserMgr.reset(new UserManager(poolContext.DatabasePath));
@@ -133,8 +154,6 @@ int main(int argc, char *argv[])
     }
 
     // Initialize all backends
-    config4cpp::StringVector coinsList;
-    cfg->lookupList(frontendSection, "coins", coinsList);
     for (int i = 0, ie = coinsList.length(); i != ie; ++i) {
       std::string scopeName = std::string("coins.") + coinsList[i];
       const char *scope = scopeName.c_str();
@@ -172,28 +191,25 @@ int main(int argc, char *argv[])
       backendConfig.StatisticCheckInterval = cfg->lookupInt(scope, "statisticCheckInterval", 1) * 60 * 1000000;
 
       // Pool fee
-      config4cpp::StringVector addresses;
-      config4cpp::StringVector fees;
-      cfg->lookupList(scope, "poolFeeAddrs", addresses, config4cpp::StringVector());
-      cfg->lookupList(scope, "poolFees", fees, config4cpp::StringVector());
-      if (addresses.length() != fees.length()) {
-        LOG_F(ERROR, "Pool fee addresses and fees arrays have different length");
-        return 1;
-      }
+      {
+        config4cpp::StringVector Names;
+        cfg->listLocallyScopedNames(scope, "fee", config4cpp::Configuration::CFG_SCOPE, false, Names);
+        for (int i = 0; i < Names.length(); i++) {
+          PoolFeeEntry poolFeeEntry;
+          char *endptr = nullptr;
+          std::string subScope = scopeName + ".fee" + "." + Names[i];
+          const char *percentage = cfg->lookupString(subScope.c_str(), "percentage");
+          poolFeeEntry.Address = cfg->lookupString(subScope.c_str(), "address");
+          poolFeeEntry.Percentage = strtof(percentage, &endptr);
+          if (!coinInfo.checkAddress(poolFeeEntry.Address, coinInfo.PayoutAddressType)) {
+            LOG_F(ERROR, "Invalid pool fee address: %s", poolFeeEntry.Address.c_str());
+            return 1;
+          }
 
-      for (int i = 0; i < addresses.length(); i++) {
-        PoolFeeEntry poolFeeEntry;
-        char *endptr = nullptr;
-        poolFeeEntry.Address = addresses[i];
-        poolFeeEntry.Percentage = strtof(fees[i], &endptr);
-        if (!coinInfo.checkAddress(poolFeeEntry.Address, coinInfo.PayoutAddressType)) {
-          LOG_F(ERROR, "Invalid pool fee address: %s", addresses[i]);
-          return 1;
-        }
-
-        if (!(endptr && *endptr == '\0')) {
-          LOG_F(ERROR, "Invalid fee value: %s", fees[i]);
-          return 1;
+          if (!(endptr && *endptr == '\0')) {
+            LOG_F(ERROR, "Invalid fee value: %s", percentage);
+            return 1;
+          }
         }
       }
 
@@ -201,7 +217,33 @@ int main(int argc, char *argv[])
       backendConfig.poolZAddr = cfg->lookupString(scope, "pool_zaddr", "");
       backendConfig.poolTAddr = cfg->lookupString(scope, "pool_taddr", "");
 
+      // Nodes
+      std::unique_ptr<CNetworkClientDispatcher> dispatcher(new CNetworkClientDispatcher);
+      {
+        config4cpp::StringVector Names;
+        cfg->listLocallyScopedNames(scope, "node", config4cpp::Configuration::CFG_SCOPE, false, Names);
+        for (int i = 0; i < Names.length(); i++) {
+          std::string subScope = scopeName + ".node" + "." + Names[i];
+          const char *type = cfg->lookupString(subScope.c_str(), "type");
+          const char *address = cfg->lookupString(subScope.c_str(), "address");
+          const char *login = cfg->lookupString(subScope.c_str(), "login", "");
+          const char *password = cfg->lookupString(subScope.c_str(), "password", "");
+
+          CNetworkClient *client;
+          if (strcmp(type, "bitcoinrpc") == 0) {
+            client = new CBitcoinRpcClient(base, totalThreadsNum, coinInfo, address, login, password);
+          } else {
+            LOG_F(ERROR, "Unknown node type: %s", type);
+            return 1;
+          }
+
+          dispatcher->addClient(client);
+        }
+      }
+
+
       // Initialize backend
+      poolContext.ClientsDispatcher.emplace_back(dispatcher.release());
       poolContext.Backends.emplace_back(std::move(backendConfig), coinInfo, *poolContext.UserMgr);
       poolContext.UserMgr->configAddCoin(coinInfo, backendConfig.DefaultPayoutThreshold);
       poolContext.CoinList.push_back(coinInfo);
@@ -212,9 +254,6 @@ int main(int argc, char *argv[])
     LOG_F(ERROR, "%s", ex.c_str());
     exit(1);
   }
-
-  initializeSocketSubsystem();
-  asyncBase *base = createAsyncBase(amOSDefault);
     
   // Start user manager
   poolContext.UserMgr->start();
@@ -224,11 +263,13 @@ int main(int argc, char *argv[])
     backend.start();
   }
 
+  // Start clients polling
+  for (auto &dispatcher: poolContext.ClientsDispatcher) {
+    dispatcher->poll();
+  }
+
   poolContext.HttpServer.reset(new PoolHttpServer(base, poolContext.HttpPort, *poolContext.UserMgr.get(), poolContext.Backends, poolContext.CoinIdxMap));
   poolContext.HttpServer->start();
-
-  if (workerThreadsNum == 0)
-    workerThreadsNum = std::thread::hardware_concurrency() ? std::thread::hardware_concurrency() : 2;
 
   std::unique_ptr<std::thread[]> workerThreads(new std::thread[workerThreadsNum]);
   for (unsigned i = 0; i < workerThreadsNum; i++) {

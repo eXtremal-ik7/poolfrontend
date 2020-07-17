3507bddf2adf385d926bb3fd84cf352671f55140
eXtremal-ik7 <eXtremal.ik7@gmail.com>
2020-07-17 15:53:08 +0300

    Use JSON for config

---
+++ src/config.h
@@ -0,0 +1,79 @@
+#pragma once
+
+#include <string>
+#include <vector>
+#include "rapidjson/document.h"
+
+enum EErrorType {
+  EOk = 0,
+  ENotExists,
+  ETypeMismatch
+};
+
+struct CInstanceConfig {
+  std::string Name;
+  std::string Type;
+  std::string Protocol;
+  std::vector<std::string> Backends;
+  rapidjson::Value InstanceConfig;
+
+  void load(rapidjson::Document &document, const rapidjson::Value &value, std::string &errorPlace, EErrorType *error);
+};
+
+struct CNodeConfig {
+  std::string Type;
+  std::string Address;
+  std::string Login;
+  std::string Password;
+
+  void load(const rapidjson::Value &value, const std::string &path, std::string &errorDescription, EErrorType *error);
+};
+
+struct CFeeConfig {
+  std::string Address;
+  float Percentage;
+
+  void load(const rapidjson::Value &value, const std::string &path, std::string &errorDescription, EErrorType *error);
+};
+
+struct CCoinConfig {
+  std::string Name;
+  std::vector<CNodeConfig> Nodes;
+  std::vector<CFeeConfig> Fees;
+  unsigned RequiredConfirmations;
+  std::string DefaultPayoutThreshold;
+  std::string MinimalAllowedPayout;
+  unsigned KeepRoundTime;
+  unsigned KeepStatsTime;
+  unsigned ConfirmationsCheckInterval;
+  unsigned PayoutInterval;
+  unsigned BalanceCheckInterval;
+  unsigned StatisticCheckInterval;
+  unsigned ShareTarget;
+  unsigned StratumWorkLifeTime;
+
+  void load(const rapidjson::Value &value, std::string &errorDescription, EErrorType *error);
+};
+
+struct CPoolFrontendConfig {
+  bool IsMaster;
+  unsigned HttpPort;
+  unsigned WorkerThreadsNum;
+  std::string DbPath;
+  std::string PoolName;
+  std::string PoolHostAddress;
+  std::string PoolActivateLinkPrefix;
+
+  bool SmtpEnabled;
+  std::string SmtpServer;
+  std::string SmtpLogin;
+  std::string SmtpPassword;
+  std::string SmtpSenderAddress;
+  bool SmtpUseSmtps;
+  bool SmtpUseStartTls;
+
+  std::vector<CCoinConfig> Coins;
+  std::vector<CInstanceConfig> Instances;
+
+  bool load(rapidjson::Document &document, std::string &errorDescription);
+};
--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,11 @@
+#include "config.h"
 #include "http.h"
 #include "poolcore/backend.h"
 #include "poolcore/bitcoinRPCClient.h"
 #include "poolcore/coinLibrary.h"
 #include "poolcore/clientDispatcher.h"
 #include "poolcommon/utils.h"
-
-#include "config4cpp/Configuration.h"
+#include "poolinstances/fabric.h"
 
 #include "asyncio/asyncio.h"
 #include "asyncio/socket.h"
@@ -75,7 +75,6 @@ int main(int argc, char *argv[])
   loguru::set_thread_name("main");
 
   PoolBackendConfig backendConfig;
-  config4cpp::Configuration *cfg = config4cpp::Configuration::create();
   PoolContext poolContext;
   unsigned totalThreadsNum = 0;
   unsigned workerThreadsNum = 0;
@@ -83,85 +82,95 @@ int main(int argc, char *argv[])
   initializeSocketSubsystem();
   asyncBase *base = createAsyncBase(amOSDefault);
 
-  try {
-    cfg->parse(argv[1]);
-    const char *frontendSection = "poolfrontend";
-    config4cpp::StringVector coinsList;
+  // Parse config
+  FileDescriptor configFd;
+  if (!configFd.open(argv[1])) {
+    LOG_F(ERROR, "Can't open config file %s", argv[1]);
+    return 1;
+  }
 
-    poolContext.IsMaster = cfg->lookupBoolean(frontendSection, "isMaster", true);
-    poolContext.DatabasePath = cfg->lookupString(frontendSection, "dbPath");
-    poolContext.HttpPort = cfg->lookupInt(frontendSection, "httpPort");
-    workerThreadsNum = cfg->lookupInt(frontendSection, "workerThreadsNum", 0);
+  std::string configData;
+  configData.resize(configFd.size());
+  configFd.read(configData.data(), 0, configData.size());
+  configFd.close();
+  rapidjson::Document document;
+  document.Parse(configData.c_str());
+  if (document.HasParseError()) {
+    LOG_F(ERROR, "Config file %s is not valid JSON", argv[1]);
+    return 1;
+  }
+
+  CPoolFrontendConfig config;
+  {
+    std::string error;
+    if (!config.load(document, error)) {
+      LOG_F(ERROR, "Config file %s contains error", argv[1]);
+      LOG_F(ERROR, "%s", error.c_str());
+      return 1;
+    }
+  }
+
+  {
+    // Analyze config
+    poolContext.IsMaster = config.IsMaster;
+    poolContext.DatabasePath = config.DbPath;
+    poolContext.HttpPort = config.HttpPort;
+    workerThreadsNum = config.WorkerThreadsNum;
     if (workerThreadsNum == 0)
       workerThreadsNum = std::thread::hardware_concurrency() ? std::thread::hardware_concurrency() : 4;
 
-    cfg->lookupList(frontendSection, "coins", coinsList);
-
-    // Calculate total threads Num
+    // Calculate total threads num
     totalThreadsNum =
-      1 +                  // Main thread
-      1 +                  // Listeners and clients polling
-      workerThreadsNum +   // Share checkers
-      coinsList.length() + // Backends
-      1;                   // HTTP server
+      1 +                   // Main thread
+      1 +                   // Listeners and clients polling
+      workerThreadsNum +    // Share checkers
+      config.Coins.size() + // Backends
+      1;
 
     // Initialize user manager
     poolContext.UserMgr.reset(new UserManager(poolContext.DatabasePath));
 
     // Base config
-    const char *poolName = cfg->lookupString(frontendSection, "poolName");
-    const char *poolHostAddress = cfg->lookupString(frontendSection, "poolHostAddress");
-    const char *userActivateLinkPrefix = cfg->lookupString(frontendSection, "poolActivateLinkPrefix");
-    poolContext.UserMgr->setBaseCfg(poolName, poolHostAddress, userActivateLinkPrefix);
+    poolContext.UserMgr->setBaseCfg(config.PoolName, config.PoolHostAddress, config.PoolActivateLinkPrefix);
 
     // SMTP config
-    if (cfg->lookupBoolean(frontendSection, "smtpEnabled", false)) {
-      HostAddress smtpAddress;
-      const char *smtpServer = cfg->lookupString(frontendSection, "smtpServer");
-      const char *login = cfg->lookupString(frontendSection, "smtpLogin");
-      const char *password = cfg->lookupString(frontendSection, "smtpPassword");
-      const char *senderAddress = cfg->lookupString(frontendSection, "smtpSenderAddress");
-      bool useSmtps = cfg->lookupBoolean(frontendSection, "smtpUseSmtps", false);
-      bool useStartTls = cfg->lookupBoolean(frontendSection, "smtpUseStartTLS", true);
-
+    if (config.SmtpEnabled) {
       // Build HostAddress for server
-      {
-        char *colonPos = (char*)strchr(smtpServer, ':');
-        if (colonPos == nullptr) {
-          LOG_F(ERROR, "Invalid server %s\nIt must have address:port format", smtpServer);
-          return 1;
-        }
-
-        *colonPos = 0;
-        hostent *host = gethostbyname(smtpServer);
-        if (!host) {
-          LOG_F(ERROR, "Cannot retrieve address of %s (gethostbyname failed)", smtpServer);
-        }
-
-        u_long addr = host->h_addr ? *reinterpret_cast<u_long*>(host->h_addr) : 0;
-        if (!addr) {
-          LOG_F(ERROR, "Cannot retrieve address of %s (gethostbyname returns 0)", smtpServer);
-          return 1;
-        }
-
-        smtpAddress.family = AF_INET;
-        smtpAddress.ipv4 = static_cast<uint32_t>(addr);
-        smtpAddress.port = htons(atoi(colonPos + 1));
+      HostAddress smtpAddress;
+      char *colonPos = (char*)strchr(config.SmtpServer.c_str(), ':');
+      if (colonPos == nullptr) {
+        LOG_F(ERROR, "Invalid server %s\nIt must have address:port format", config.SmtpServer.c_str());
+        return 1;
       }
 
+      *colonPos = 0;
+      hostent *host = gethostbyname(config.SmtpServer.c_str());
+      if (!host) {
+        LOG_F(ERROR, "Cannot retrieve address of %s (gethostbyname failed)", config.SmtpServer.c_str());
+      }
+
+      u_long addr = host->h_addr ? *reinterpret_cast<u_long*>(host->h_addr) : 0;
+      if (!addr) {
+        LOG_F(ERROR, "Cannot retrieve address of %s (gethostbyname returns 0)", config.SmtpServer.c_str());
+        return 1;
+      }
+
+      smtpAddress.family = AF_INET;
+      smtpAddress.ipv4 = static_cast<uint32_t>(addr);
+      smtpAddress.port = htons(atoi(colonPos + 1));
+
       // Enable SMTP
-      poolContext.UserMgr->enableSMTP(smtpAddress, login, password, senderAddress, useSmtps, useStartTls);
+      poolContext.UserMgr->enableSMTP(smtpAddress, config.SmtpLogin, config.SmtpPassword, config.SmtpSenderAddress, config.SmtpUseSmtps, config.SmtpUseStartTls);
     }
 
     // Initialize all backends
-    for (int i = 0, ie = coinsList.length(); i != ie; ++i) {
-      std::string scopeName = std::string("coins.") + coinsList[i];
-      const char *scope = scopeName.c_str();
-
+    for (size_t coinIdx = 0, coinIdxE = config.Coins.size(); coinIdx != coinIdxE; ++coinIdx) {
       PoolBackendConfig backendConfig;
-      CCoinInfo coinInfo = CCoinLibrary::get(coinsList[i]);
+      const CCoinConfig &coinConfig = config.Coins[coinIdx];
+      const char *coinName = coinConfig.Name.c_str();
+      CCoinInfo coinInfo = CCoinLibrary::get(coinName);
       if (coinInfo.Name.empty()) {
-        LOG_F(ERROR, "Unknown coin: %s", coinsList[i]);
+        LOG_F(ERROR, "Unknown coin: %s", coinName);
         return 1;
       }
 
@@ -170,89 +179,70 @@ int main(int argc, char *argv[])
       backendConfig.dbPath = poolContext.DatabasePath;
 
       // Backend parameters
-      const char *defaultPayoutThresholdS = cfg->lookupString(scope, "defaultPayoutThreshold");
-      if (!parseMoneyValue(defaultPayoutThresholdS, coinInfo.RationalPartSize, &backendConfig.DefaultPayoutThreshold)) {
-        LOG_F(ERROR, "Can't load 'defaultPayoutThreshold' from %s coin config", coinsList[i]);
+      if (!parseMoneyValue(coinConfig.DefaultPayoutThreshold.c_str(), coinInfo.RationalPartSize, &backendConfig.DefaultPayoutThreshold)) {
+        LOG_F(ERROR, "Can't load 'defaultPayoutThreshold' from %s coin config", coinName);
         return 1;
       }
 
-      const char *mininalAllowedPayout = cfg->lookupString(scope, "minimalAllowedPayout");
-      if (!parseMoneyValue(mininalAllowedPayout, coinInfo.RationalPartSize, &backendConfig.MinimalAllowedPayout)) {
-        LOG_F(ERROR, "Can't load 'minimalPayout' from %s coin config", coinsList[i]);
+      if (!parseMoneyValue(coinConfig.MinimalAllowedPayout.c_str(), coinInfo.RationalPartSize, &backendConfig.MinimalAllowedPayout)) {
+        LOG_F(ERROR, "Can't load 'minimalPayout' from %s coin config", coinName);
         return 1;
       }
 
-      backendConfig.RequiredConfirmations = cfg->lookupInt(scope, "requiredConfirmations", 10);
-      backendConfig.KeepRoundTime = cfg->lookupInt(scope, "keepRoundTime", 3) * 24*3600;
-      backendConfig.KeepStatsTime = cfg->lookupInt(scope, "keepStatsTime", 2) * 60;
-      backendConfig.ConfirmationsCheckInterval = cfg->lookupInt(scope, "confirmationsCheckInterval", 10) * 60 * 1000000;
-      backendConfig.PayoutInterval = cfg->lookupInt(scope, "payoutInterval", 60) * 60 * 1000000;
-      backendConfig.BalanceCheckInterval = cfg->lookupInt(scope, "balanceCheckInterval", 3) * 60 * 1000000;
-      backendConfig.StatisticCheckInterval = cfg->lookupInt(scope, "statisticCheckInterval", 1) * 60 * 1000000;
+      backendConfig.RequiredConfirmations = coinConfig.RequiredConfirmations;
+      backendConfig.KeepRoundTime = coinConfig.KeepRoundTime * 24*3600;
+      backendConfig.KeepStatsTime = coinConfig.KeepStatsTime * 60;
+      backendConfig.ConfirmationsCheckInterval = coinConfig.ConfirmationsCheckInterval * 60 * 1000000;
+      backendConfig.PayoutInterval = coinConfig.PayoutInterval * 60 * 1000000;
+      backendConfig.BalanceCheckInterval = coinConfig.BalanceCheckInterval * 60 * 1000000;
+      backendConfig.StatisticCheckInterval = coinConfig.StatisticCheckInterval * 60 * 1000000;
 
-      // Pool fee
-      {
-        config4cpp::StringVector Names;
-        cfg->listLocallyScopedNames(scope, "fee", config4cpp::Configuration::CFG_SCOPE, false, Names);
-        for (int i = 0; i < Names.length(); i++) {
-          PoolFeeEntry poolFeeEntry;
-          char *endptr = nullptr;
-          std::string subScope = scopeName + ".fee" + "." + Names[i];
-          const char *percentage = cfg->lookupString(subScope.c_str(), "percentage");
-          poolFeeEntry.Address = cfg->lookupString(subScope.c_str(), "address");
-          poolFeeEntry.Percentage = strtof(percentage, &endptr);
-          if (!coinInfo.checkAddress(poolFeeEntry.Address, coinInfo.PayoutAddressType)) {
-            LOG_F(ERROR, "Invalid pool fee address: %s", poolFeeEntry.Address.c_str());
-            return 1;
-          }
+      backendConfig.PoolFee.resize(coinConfig.Fees.size());
+      for (size_t feeIdx = 0, feeIdxE = coinConfig.Fees.size(); feeIdx != feeIdxE; ++feeIdx) {
+        PoolFeeEntry &entry = backendConfig.PoolFee[feeIdx];
+        entry.Address = coinConfig.Fees[feeIdx].Address;
+        entry.Percentage = coinConfig.Fees[feeIdx].Percentage;
+        if (!coinInfo.checkAddress(entry.Address, coinInfo.PayoutAddressType)) {
+          LOG_F(ERROR, "Invalid pool fee address: %s", entry.Address.c_str());
+          return 1;
+        }
 
-          if (!(endptr && *endptr == '\0')) {
-            LOG_F(ERROR, "Invalid fee value: %s", percentage);
-            return 1;
-          }
+        if (entry.Percentage > 100.0f) {
+          LOG_F(ERROR, "Invalid pool fee: %.3f", entry.Percentage);
+          return 1;
         }
       }
 
       // ZEC specific
-      backendConfig.poolZAddr = cfg->lookupString(scope, "pool_zaddr", "");
-      backendConfig.poolTAddr = cfg->lookupString(scope, "pool_taddr", "");
+      // backendConfig.poolZAddr;
+      // backendConfig.poolTAddr;
 
       // Nodes
       std::unique_ptr<CNetworkClientDispatcher> dispatcher(new CNetworkClientDispatcher);
-      {
-        config4cpp::StringVector Names;
-        cfg->listLocallyScopedNames(scope, "node", config4cpp::Configuration::CFG_SCOPE, false, Names);
-        for (int i = 0; i < Names.length(); i++) {
-          std::string subScope = scopeName + ".node" + "." + Names[i];
-          const char *type = cfg->lookupString(subScope.c_str(), "type");
-          const char *address = cfg->lookupString(subScope.c_str(), "address");
-          const char *login = cfg->lookupString(subScope.c_str(), "login", "");
-          const char *password = cfg->lookupString(subScope.c_str(), "password", "");
-
-          CNetworkClient *client;
-          if (strcmp(type, "bitcoinrpc") == 0) {
-            client = new CBitcoinRpcClient(base, totalThreadsNum, coinInfo, address, login, password);
-          } else {
-            LOG_F(ERROR, "Unknown node type: %s", type);
-            return 1;
-          }
-
-          dispatcher->addClient(client);
+      for (size_t nodeIdx = 0, nodeIdxE = coinConfig.Nodes.size(); nodeIdx != nodeIdxE; ++nodeIdx) {
+        CNetworkClient *client;
+        const CNodeConfig &node = coinConfig.Nodes[nodeIdx];
+        if (node.Type == "bitcoinrpc") {
+          client = new CBitcoinRpcClient(base, totalThreadsNum, coinInfo, node.Address.c_str(), node.Login.c_str(), node.Password.c_str());
+        } else {
+          LOG_F(ERROR, "Unknown node type: %s", node.Type.c_str());
+          return 1;
         }
-      }
 
+        dispatcher->addClient(client);
+      }
 
       // Initialize backend
       poolContext.ClientsDispatcher.emplace_back(dispatcher.release());
       poolContext.Backends.emplace_back(std::move(backendConfig), coinInfo, *poolContext.UserMgr);
       poolContext.UserMgr->configAddCoin(coinInfo, backendConfig.DefaultPayoutThreshold);
       poolContext.CoinList.push_back(coinInfo);
-      poolContext.CoinIdxMap[coinsList[i]] = i;
+      poolContext.CoinIdxMap[coinName] = coinIdx;
     }
 
-  } catch(const config4cpp::ConfigurationException& ex){
-    LOG_F(ERROR, "%s", ex.c_str());
-    exit(1);
+    for (size_t instIdx = 0, instIdxE = config.Instances.size(); instIdx != instIdxE; ++instIdx) {
+
+    }
   }
     
   // Start user manager
